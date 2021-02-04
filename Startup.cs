5266477cded3a96f783d15a0f981c61d7fa71656
quangduccao <onggieoi@gmail.com>
2021-02-04 10:58:00 +0700

    add gitignoge

--- Startup.cs
+++ Startup.cs
@@ -12,25 +12,37 @@ using Microsoft.Extensions.Configuration;
 
 namespace Platform
 {
-    public class Startup
+  public class Startup
+  {
+    private IConfiguration Configuration { get; set; }
+    public Startup(IConfiguration config)
     {
-        private IConfiguration Configuration;
-        public Startup(IConfiguration config)
-        {
-            Configuration = config;
-        }
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddSingleton(typeof(ICollection<>), typeof(List<>));
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            app.UseDeveloperExceptionPage();
-            app.UseRouting();
-        }
+      Configuration = config;
     }
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddSingleton(typeof(ICollection<>), typeof(List<>));
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      app.UseDeveloperExceptionPage();
+      app.UseRouting();
+
+      app.Use(async (context, next) =>
+      {
+        string defaultDebug = Configuration["Logging:LogLevel:Default"];
+        await context.Response
+        .WriteAsync($"The config setting is: {defaultDebug}");
+      });
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapGet("/", async context => await context.Response.WriteAsync("Hello world"));
+      });
+    }
+  }
 }
